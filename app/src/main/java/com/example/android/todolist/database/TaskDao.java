ac2ea3b432612f79cf1463f1cf7c0384f7db42bf
DUMA042 <dumebiikeoffiah@gmail.co>
2020-09-10 14:17:50 +0300

    Shows how to Use room Basics with Excutor

---
+++ app/src/main/java/com/example/android/todolist/database/TaskDao.java
@@ -0,0 +1,24 @@
+package com.example.android.todolist.database;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+@Dao
+public interface TaskDao {
+    @Query("SELECT * FROM task ORder BY priority")
+    List<TaskEntry> loadAllEntity();
+@Insert
+    void InsertTask(TaskEntry taskEntry );
+@Update(onConflict=OnConflictStrategy.REPLACE)
+    void UpdateTask(TaskEntry taskEntry);
+@Delete
+    void DeleteTask(TaskEntry taskEntry);
+    @Query("SELECT * FROM task Where id=:id")
+TaskEntry loadTaskById(int id);
+
+}
